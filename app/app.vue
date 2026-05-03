517e70f52189e989fe832f2779a97d54915395fa
Jaron <jaron@ed.link>
2026-05-02 21:04:04 -0500

    refactor: comment out unused font declarations in app and stylesheet files

--- app/app.vue
+++ app/app.vue
@@ -4,20 +4,20 @@ import '~/assets/css/main.css';
 useHead({
     link: [
         { rel: 'icon', type: 'image/png', href: '/favicon.png' },
-        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
-        {
-            rel: 'preconnect',
-            href: 'https://fonts.gstatic.com',
-            crossorigin: '',
-        },
-        {
-            rel: 'stylesheet',
-            href: 'https://fonts.googleapis.com/css2?family=Baskervville:ital,wght@0,400..700;1,400..700&display=swap',
-        },
-        {
-            rel: 'stylesheet',
-            href: 'https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400..800;1,400..800&display=swap',
-        },
+        // { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        // {
+        //     rel: 'preconnect',
+        //     href: 'https://fonts.gstatic.com',
+        //     crossorigin: '',
+        // },
+        // {
+        //     rel: 'stylesheet',
+        //     href: 'https://fonts.googleapis.com/css2?family=Baskervville:ital,wght@0,400..700;1,400..700&display=swap',
+        // },
+        // {
+        //     rel: 'stylesheet',
+        //     href: 'https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400..800;1,400..800&display=swap',
+        // },
     ],
 });
 
