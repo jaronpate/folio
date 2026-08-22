2f076b01359f18d0d17c10aba42925a0386725e1
Jaron <jaron@hat.fish>
2026-08-22 13:18:37 -0500

    hide writing for now

--- app/pages/writing/index.vue
+++ app/pages/writing/index.vue
@@ -7,10 +7,13 @@ import {
     websiteId,
 } from '~/utils/site';
 
-const { data: pages } = await useAsyncData('writing-pages', async () => {
-    const all = await queryCollection('writing').order('date', 'DESC').all();
-    return import.meta.dev ? all : all.filter((page) => !page.demo);
-});
+// TODO: Uncomment when ready to show writing
+// const { data: pages } = await useAsyncData('writing-pages', async () => {
+//     const all = await queryCollection('writing').order('date', 'DESC').all();
+//     return import.meta.dev ? all : all.filter((page) => !page.demo);
+// });
+
+const pages = ref<any[]>([]);
 
 const writingUrl = `${SITE_URL}/writing`;
 const collectionId = `${writingUrl}#collection`;
